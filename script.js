bc43ebcd1c73f0e50874a3abab70ef1d6d7123ea
Marly Ammerlaan <marly.ammerlaan@gmail.com>
2024-02-03 23:11:17 +0100

    Play the game 5 times

--- script.js
+++ script.js
@@ -2,11 +2,6 @@
 let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
-let playerChoice = prompt("Enter Rock, Paper, or Scissors");
-
-// Paramaters:
-playerSelection = playerChoice.toLowerCase();
-computerSelection = getComputerChoice();
 
 // Computer will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
 function getComputerChoice (){
@@ -21,29 +16,38 @@ function getComputerChoice (){
     }
 }
 
+// times won / lost variable
+let computerWin = 0;
+let playerWin = 0;
+
+
 // game
 function game (playerSelection, computerSelection){
-    console.log(computerSelection);
-
     if (playerSelection === rock && computerSelection === rock || playerSelection === paper && computerSelection === paper || playerSelection === scissors && computerSelection === scissors){
         return "Tied";
     }
     else if (playerSelection === rock && computerSelection === scissors){
+        playerWin = playerWin++;
         return "You won! Rock beats scissors."
     }
     else if (playerSelection === rock && computerSelection === paper){
+        computerWin = computerWin++;
         return "You lost! Paper beats rock."
     }
     else if (playerSelection === paper && computerSelection === rock){
+        playerWin = playerWin++;
         return "You won! Paper beats Rock.";
     }
     else if (playerSelection === paper && computerSelection === scissors){
+        computerWin = computerWin++;
         return "You lost! Scissors beats Paper.";
     }
     else if (playerSelection === scissors && computerSelection === paper){
+        playerWin = playerWin++;
         return "You won! Scissors beats Paper.";
     }
     else if (playerSelection === scissors && computerSelection === rock){
+        computerWin = computerWin++;
         return "You lost! Rock beats scissors.";
     }
     else {
@@ -51,8 +55,21 @@ function game (playerSelection, computerSelection){
     }
 }
 
-console.log(game(playerSelection, computerSelection))
 
+// Who wins after 5 games
 function playGame(){
-
+    for (let i = 0; i < 5; i++) {
+        i = i++
+        // Choices from player and computer
+        let playerChoice = prompt("Enter Rock, Paper, or Scissors");
+        playerSelection = playerChoice.toLowerCase();
+        computerSelection = getComputerChoice();
+        // Run function
+        console.log(game(playerSelection, computerSelection))
+      }
+    
+    console.log(playerWin);
+    console.log(computerWin);
 }
+
+console.log(playGame());
